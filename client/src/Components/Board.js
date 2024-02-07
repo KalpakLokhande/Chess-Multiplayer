453dc8c4137866eebaea4f6d1010da8adab64a3f
KalpakLokhande <102809949+KalpakLokhande@users.noreply.github.com>
2024-02-07 22:14:16 +0530

    minor changes

---
+++ client/src/Components/Board.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Board = () => {
+
+    const [FEN,setFEN] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+
+  return (
+    <div className='board' >
+      
+    </div>
+  )
+}
+
+export default Board
